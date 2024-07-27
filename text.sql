44ea72e63b0bdead4d437b669103da93b6ba5f67
Margosha2002 <margosha161616@gmail.com>
2024-07-28 00:00:37 +0100

    hw3

---
+++ text.sql
@@ -0,0 +1,24 @@
+SELECT * FROM products;
+
+SELECT name, phone
+FROM shippers;
+
+SELECT AVG(price) AS average_price, MAX(price) AS max_price, MIN(price) AS min_price
+FROM products;
+
+
+SELECT DISTINCT category_id, price
+FROM products
+ORDER BY price DESC
+LIMIT 10;
+
+SELECT COUNT(*)
+FROM products
+WHERE price BETWEEN 20 AND 100;
+
+SELECT supplier_id, COUNT(*) AS product_count, AVG(price) AS average_price
+FROM products
+GROUP BY supplier_id;
+
+
+
